6ae4e5fc8ebd9d7f302eff854e51d311e1bc0e3f
Xiangyun Huang <xiangyunfaith@outlook.com>
2019-06-11 16:14:24 +0100

    升级系列软件环境
    Pandoc 升级到 2.7.2，g++ 升级到 8
    
    添加 sidebar 环境，定制警告环境， TODO 环境

---
+++ sidebar.lua
@@ -0,0 +1,27 @@
+-- https://github.com/jgm/pandoc/issues/2106#issuecomment-371355862
+function Span(el)
+  if el.classes:includes("todo") then
+    return {
+      pandoc.RawInline("latex", "\\textcolor{red}{\\textbf{TODO: }"),
+      el,
+      pandoc.RawInline("latex", "}")
+    }
+  end
+end
+
+function Div(el)
+  if el.classes:includes("sidebar") then
+    return {
+      pandoc.RawBlock("latex", "\\begin{shaded}"),
+      el,
+      pandoc.RawBlock("latex", "\\end{shaded}")
+    }
+  end
+  if el.classes:includes("warning") then
+    return {
+      pandoc.RawBlock("latex", "\\begin{shaded}\\textcolor{red}{\\textbf{警告}}"),
+      el,
+      pandoc.RawBlock("latex", "\\end{shaded}")
+    }
+  end
+end
