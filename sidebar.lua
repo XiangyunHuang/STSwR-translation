c4642fb509654d7b39708531abc1cd4b23e72b80
Xiangyun Huang <xiangyunfaith@outlook.com>
2019-06-13 14:14:38 +0100

    定制警告、提示和注意三个 block 样式

--- sidebar.lua
+++ sidebar.lua
@@ -17,9 +17,23 @@ function Div(el)
       pandoc.RawBlock("latex", "\\end{shaded}")
     }
   end
-  if el.classes:includes("warning") then
+  if el.classes:includes("rmdwarning") then
     return {
-      pandoc.RawBlock("latex", "\\begin{shaded}\\textcolor{red}{\\textbf{警告}}"),
+      pandoc.RawBlock("latex", "\\begin{shaded}\\textcolor[RGB]{234,67,53}{\\textbf{警告}}"),
+      el,
+      pandoc.RawBlock("latex", "\\end{shaded}")
+    }
+  end
+  if el.classes:includes("rmdnote") then
+    return {
+      pandoc.RawBlock("latex", "\\begin{shaded}\\textcolor[RGB]{251,188,5}{\\textbf{注意}}"),
+      el,
+      pandoc.RawBlock("latex", "\\end{shaded}")
+    }
+  end
+  if el.classes:includes("rmdtip") then
+    return {
+      pandoc.RawBlock("latex", "\\begin{shaded}\\textcolor[RGB]{52,168,83}{\\textbf{提示}}"),
       el,
       pandoc.RawBlock("latex", "\\end{shaded}")
     }
